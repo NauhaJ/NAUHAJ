98180619c9a6a390cb4df1ed0dd32a593a90fc50
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:21:33 +0300

    Musiikkikappale lisätty

---
+++ C# gradia/Musiikkikappale/Musiikkikappale.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musiikki
+{
+    // Luo projektiin luokka nimeltä Musiikkikappale
+    internal class Musiikkikappale
+    {
+        //  Musiikkikappaleella on oliomuuttujat nimi (merkkijono) ja pituus sekunteina (kokonaisluku)
+        private string kappale_nimi = "";
+        private int kappale_pituus = 0;
+
+        // olemmat asetetaan konstruktorissa public Musiikkikappale(String kappaleenNimi, int kappaleenPituus)
+        public Musiikkikappale(String kappaleenNimi, int kappaleenPituus)
+        {
+            this.kappale_nimi = kappaleenNimi;
+            this.kappale_pituus = kappaleenPituus;
+        }
+        // Lisää oliolle myös metodi public string Nimi(), joka palauttaa kappaleen nimen
+        public string Nimi()
+        {
+            return this.kappale_nimi;
+        }
+        // Lisää oliolle metodi public int Pituus(), joka palauttaa kappaleen pituuden.
+        public int Pituus()
+        {
+            return this.kappale_pituus;
+        }
+    }
+}
---
+++ C# gradia/Musiikkikappale/Program.cs
@@ -0,0 +1,21 @@
+﻿using Musiikki;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musiikki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musiikkikappale garden = new Musiikkikappale("In The Garden", 10910);
+            Console.WriteLine($"Kappaleen { garden.Nimi()} pituus on { garden.Pituus()} sekuntia.");
+            Musiikkikappale metallica = new Musiikkikappale("Enter Sandman", 12345);
+            Console.WriteLine($"Kappaleen {metallica.Nimi()} pituus on {metallica.Pituus()} sekuntia.");
+
+        }
+    }
+}
