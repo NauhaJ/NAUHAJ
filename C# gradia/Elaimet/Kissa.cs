d80dcf1b662cc5e8f7c26b8de51d9f6b8fc4ea58
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:16:09 +0300

    eläimet tehtävä lisätty

---
+++ C# gradia/Elaimet/Kissa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaimet
+{
+    class Kissa
+    {
+        private int Ika;
+        public string Nimi;
+
+        public Kissa()
+        {
+            Nimi = "";
+            Ika = 15;
+        }
+        public Kissa(int Ika, string Nimi)
+        {
+            this.Ika = Ika;
+            this.Nimi = Nimi;
+        }
+        public void  AsetaKissanNimi(string Nimi)
+        {
+            this.Nimi = Nimi;
+        }
+        public string PalautaKissanNimi()
+        {
+            return Nimi;
+        }
+        public int PalautaKissanIka()
+        {
+            return Ika;
+        }
+
+    }
+}
+
