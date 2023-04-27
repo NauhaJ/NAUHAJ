4a5ca9d07e8cb0db06cebf769f51c8bf378e13d3
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:20:23 +0300

    Mittatehtävä lisätty

---
+++ C# gradia/MittaTehtävä/Mitta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MittaTehtävä
+{
+    // Luo projektiin luokka Mittari
+    internal class Mittari
+    {
+        // Mittarilla on oliomuuttuja private int _mitta 
+        private int _mitta = 0;
+
+        // parametriton konstruktori (asettaa muuttujan mitta alkuarvoksi 0)
+        public Mittari()
+        {
+            _mitta = 0;
+        }
+
+        // Metodi public void Lisaa() kasvattaa oliomuuttujan _mitta arvoa yhdellä. Ei kasvata arvoa yli viiden
+        public void Lisaa()
+        {
+            if (_mitta < 5)
+            {
+                _mitta++;       // Kasvattaa _mitta arvoa yhdellä
+            }
+        }
+        // Metodi public void Vahenna() vähentää oliomuuttujan _mitta arvoa yhdellä. Ei vähennä arvoa negatiiviseksi
+        public void Vahenna()
+        {
+            if ( _mitta > 0)
+            {
+                _mitta--;       // Vähentää _mitta arvoa yhdellä
+            }
+            
+        }
+        // Metodi public int Mitta() palauttaa oliomuuttujan _mitta arvon
+        public int Mitta()
+        {
+            return _mitta;          // palauttaa _mitta arvon
+        }
+        //Metodi public boolean Taynna() palauttaa true mikäli oliomuuttujan _mitta on viisi, palauttaa muulloin false.
+        public bool Taynna()
+        {
+            if (_mitta == 5)        // Jos mitta on sama kuin viisi 
+            {
+                return true;        // palauta TRUE
+            }
+            else                    
+            { 
+                return false;       // muuten palauttaa FALSE
+            }
+
+        }
+
+    }
+}
---
+++ C# gradia/MittaTehtävä/Program.cs
@@ -0,0 +1,33 @@
+﻿using MittaTehtävä;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MittaTehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // luodaan mittari1 olio
+            Mittari mittari1 = new Mittari();
+
+            // huomaa huutomerkki joka muuttaa totuusarvon päinvastaiseksi. Silmukka kiertää koodilohkon läpi niin kauan kuin määritetty ehto täyttyy
+            while (!mittari1.Taynna())          
+            {
+                Console.WriteLine($"Ei täynnä! Mitta: {mittari1.Mitta()}");
+                // kasvattaa arvoa yhdellä joka kierroksella
+                mittari1.Lisaa();
+            }
+            // Hypätään silmukasta pois ja tulostaa ilmoituksen ja mitan arvon
+            Console.WriteLine($"Täynnä! Mitta: {mittari1.Mitta()}");
+            // vähentää yhdellä 
+            mittari1.Vahenna();
+            // Tulostaa nykyisen mitan arvon
+            Console.WriteLine($"Ei täynnä! Mitta: {mittari1.Mitta()}");
+            Console.ReadLine();
+        }
+    }
+}
