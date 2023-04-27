244e4e7b4f5af6b98e577296f27802b3fce5d2fa
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:19:27 +0300

    Maksukortti tehtävä lisätty

---
+++ C# gradia/MaksuKortti/Program.cs
@@ -0,0 +1,40 @@
+﻿using maksuKortti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Paaohjelma
+{
+    public static void Main(String[] args)
+    {
+        //Maksukortti kortti = new Maksukortti(50);
+        //Console.WriteLine(kortti);
+        //kortti.syoEdullisesti();
+        //Console.WriteLine(kortti);
+        //kortti.syoMaukkaasti();
+        //kortti.syoEdullisesti();
+        //Console.WriteLine(kortti);
+        //Maksukortti kortti = new Maksukortti(5);
+        //Console.WriteLine(kortti);
+        //kortti.syoMaukkaasti();
+        //Console.WriteLine(kortti);
+        //kortti.syoMaukkaasti();
+        //Console.WriteLine(kortti);
+        //Maksukortti kortti = new Maksukortti(10);
+        //Console.WriteLine(kortti);
+        //kortti.LataaRahaa(15);
+        //Console.WriteLine(kortti);
+        //kortti.LataaRahaa(10);
+        //Console.WriteLine(kortti);
+        //kortti.LataaRahaa(200);
+        //Console.WriteLine(kortti);
+
+        //Maksukortti kortti = new Maksukortti(10);
+        //Console.WriteLine("Pekka: " + kortti);
+        //kortti.LataaRahaa(-15);
+        //Console.WriteLine("Pekka: " + kortti);
+    }
+}
---
+++ C# gradia/MaksuKortti/maksukortti.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maksuKortti
+{
+    // tehdään luokka Maksukortti
+    public class Maksukortti
+    {
+        private double saldo;
+        public Maksukortti(double alkusaldo)
+        {
+            this.saldo = alkusaldo;
+        }
+        // Metodin syoEdullisesti tulisi vähentää kortin saldoa 2.60 eurolla
+        public void syoEdullisesti()
+        {
+            // luodaan muuttuja aterianhinta ja annetaan aterialle hinta
+            double edullinen_hinta = 2.6;
+
+            this.saldo = (this.saldo - edullinen_hinta);
+        }
+
+        // metodin syoMaukkaasti tulisi vähentää kortin saldoa 4.60 eurolla
+        public void syoMaukkaasti()
+        {
+            // luodaan muuttuja aterianhinta ja annetaan aterialle hinta
+            double maukas_hinta = 4.6;
+            // verrataan onko saldoa enempi kuin aterian hinta 4.6
+            if (this.saldo > maukas_hinta )
+            {
+                this.saldo = (this.saldo - maukas_hinta);
+            }
+        //    // Jos menee negatiiviseksi niin palauttaa olemassa olevan saldon
+        //    else
+        //    {
+        //        this.saldo = this.saldo;
+        //    }
+        }
+        public void LataaRahaa(double saldo)
+        {
+            if (saldo > 0)
+            {
+                this.saldo = saldo + this.saldo;
+            }
+        }
+
+        
+        public override string ToString()
+        {
+            return ($"Kortilla on rahaa {this.saldo} euroa");
+        }
+    }
+}
