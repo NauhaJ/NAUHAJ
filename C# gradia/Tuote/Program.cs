14388e33b70e6aa73bdd21698d98d049426b6ac7
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:22:49 +0300

    Tuote lisätty

---
+++ C# gradia/Tuote/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kauppa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Tuote kauppatuote = new Tuote("Banaani",1.1,13);    // tulostus olettaa, että tuotteen nimeksi on annettu banaani, hinnaksi on annettu 1.1, ja määräksi on annettu 13.
+            kauppatuote.TulostaTuote();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# gradia/Tuote/Tuote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kauppa
+{
+    class Tuote         // Luo projektiin luokka Tuote, joka esittää kaupan tuotetta
+    {                   // Tuotteella tulee olla hinta (double), lukumäärä (int) ja nimi (String).
+        public double hinta;
+        public int lukumaara;
+        public string nimi;
+
+        public Tuote(String nimiAlussa, double hintaAlussa, int maaraAlussa)        // Konstruktori public Tuote(String nimiAlussa, double hintaAlussa, int maaraAlussa) 
+        {
+            nimi = nimiAlussa;
+            hinta = hintaAlussa;
+            lukumaara = maaraAlussa;
+        }
+        public void TulostaTuote()          // Metodi public void TulostaTuote() joka tulostaa tuotteen tiedot
+        {
+            Console.WriteLine($"{nimi}, Hinta: {hinta} euroa, {lukumaara} kpl.");        // Esimerkkitulostus Banaani, hinta 1.1, 13 kpl
+
+        }
+    }
+}
+
