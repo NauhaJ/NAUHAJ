31eca911677f3780dde3c6912e03f5717279dee5
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:22:08 +0300

    OVI lisätty

---
+++ C# gradia/ovi/Ovi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakenne
+{
+    class Ovi       // Luo projektiin luokka nimeltä Ovi
+    {               // Ovella ei ole oliomuuttujia.
+
+        public Ovi()    // Luo sille parametriton konstruktori (tai käytä oletuskonstruktoria)
+        {
+        }
+        public void koputa()        // Luo tämän jälkeen ovelle metodi public void koputa()
+        {
+            Console.WriteLine("Who's there?");      // joka kutsuttaessa tulostaa viestin "Who's there?".
+        }
+    }
+}
---
+++ C# gradia/ovi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakenne
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Ovi alexander = new Ovi();      // Luodaan uusi olio
+            alexander.koputa();             // Kutsuu metodia
+            alexander.koputa();
+            Console.ReadLine();             // lukee rivit
+        }
+    }
+}
+
