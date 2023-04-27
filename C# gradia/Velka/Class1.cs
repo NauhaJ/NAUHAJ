fc0716b5f8a4085b2c9784aedc52393b79261819
NauhaJ <janne.nauha@gmail.com>
2023-04-27 18:23:37 +0300

    Velka lisätty

---
+++ C# gradia/Velka/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raha
+{
+    class Velka                                     // Luo projektiin luokka Velka
+    {
+        private double _saldo = 0.0;                  // jolla on double-tyyppiset oliomuuttujat _saldo ja _korkoProsentti
+        private double _korkoProsentti = 0.0;
+
+        public Velka(double saldoAlussa, double korkoProsentti)         // Saldo ja korkokerroin annetaan konstruktorin parametrina public Velka(double saldoAlussa, double korkoProsentti).
+        {
+            _saldo = saldoAlussa;
+            _korkoProsentti = korkoProsentti;
+        }
+        public void TulostaSaldo()                  // Luo luokalle myös metodit public void TulostaSaldo()
+        {
+            Console.WriteLine($"Lainaa on {_saldo} euroa.");
+        }
+        public void OdotaVuosi()                    // public void OdotaVuosi()                
+        {
+            _saldo = _saldo * (1 +(_korkoProsentti / 100));         // Lasketaan korkoprosentilla uusi saldo
+        }
+    }
+}
---
+++ C# gradia/Velka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Raha
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Velka laina = new Velka(1000, 5);       // Luodaan uusi olio LAINA
+            laina.TulostaSaldo();                   // tulosta Saldo
+            laina.OdotaVuosi();
+            laina.TulostaSaldo();                   // tulosta uusi Saldo
+            Console.Read();
+        }
+    }
+}
+
