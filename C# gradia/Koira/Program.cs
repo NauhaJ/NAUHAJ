6300e1fdf951d997258f6f800b137a64976b8662
NauhaJ <janne.nauha@gmail.com>
2023-05-04 17:07:12 +0300

    Koira lisätty

---
+++ C# gradia/Koira/Program.cs
@@ -0,0 +1,9 @@
+﻿using koira;
+
+Koira Turre = new Koira();
+Turre.AsetaKoiranNimi("Max");
+Console.WriteLine($"Koiran nimi on: {Turre.PalautaKoiranNimi()}");
+Turre.AsetaKoiranNimi("musti");
+Console.WriteLine($"musti nimellä boolean totuusarvo on: {Turre.AsetaKoiranNimi("musti")}");
+Console.WriteLine($"Koiran nimi on: {Turre.PalautaKoiranNimi()}");
+Console.ReadLine();
