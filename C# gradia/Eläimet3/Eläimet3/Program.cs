f770a2d172d2d41c8a0997e1ce2e9c777669ed82
NauhaJ <janne.nauha@gmail.com>
2023-05-09 11:51:07 +0300

    Lisätty Eläimet3 ja eläinluokka

--- C# gradia/Eläimet3/Eläimet3/Program.cs
+++ C# gradia/Eläimet3/Eläimet3/Program.cs
@@ -7,55 +7,55 @@ namespace Elainluokat
     {
         static void Main(string[] args)
         {
-            ////Tässä luon Hevonen luokasta olion
-            //Hevonen Heppa = new Hevonen();
-            //Heppa.Nimi = "Histamiini";
-            //Heppa.Paino = 89;
+            //Tässä luon Hevonen luokasta olion
+            Hevonen Heppa = new Hevonen();
+            Heppa.Nimi = "Histamiini";
+            Heppa.Paino = 89;
 
-            ////Tulosta Hevonen olion nimi ja paino
-            //Console.WriteLine(Heppa.Nimi + " " + Heppa.Paino);
+            //Tulosta Hevonen olion nimi ja paino
+            Console.WriteLine(Heppa.Nimi + " " + Heppa.Paino);
 
-            ////Luodaan uusi kissa-olio parametrittomalla konstruktorilla. 
-            //Kissa Kissa1 = new Kissa();
+            //Luodaan uusi kissa-olio parametrittomalla konstruktorilla. 
+            Kissa Kissa1 = new Kissa();
 
-            //// Tulosta kissan nimi
-            //Console.WriteLine($"Kissan nimi alussa {Kissa1.PalautaKissanNimi()}.");
+            // Tulosta kissan nimi
+            Console.WriteLine($"Kissan nimi alussa {Kissa1.PalautaKissanNimi()}.");
 
-            //// Tulosta kissan ikä
-            //Console.WriteLine($"Kissan ikä alussa {Kissa1.PalautaKissanIka()}.");
+            // Tulosta kissan ikä
+            Console.WriteLine($"Kissan ikä alussa {Kissa1.PalautaKissanIka()}.");
 
-            //// uusi kissa-olio ja anna ikä ja nimi konstruktorissa
-            //Kissa Kolli = new Kissa(8, "Mauri");
+            // uusi kissa-olio ja anna ikä ja nimi konstruktorissa
+            Kissa Kolli = new Kissa(8, "Mauri");
 
-            //// Tulostaa Kollin nimen ja iän
-            //Console.WriteLine(Kolli.PalautaKissanNimi() + " on " + Kolli.PalautaKissanIka() + " vuotta vanha kokenut kissa.");
+            // Tulostaa Kollin nimen ja iän
+            Console.WriteLine(Kolli.PalautaKissanNimi() + " on " + Kolli.PalautaKissanIka() + " vuotta vanha kokenut kissa.");
 
-            //// Annetaan Kollille negatiivinen ika konsturkorissa
-            //Kolli.AsetaKissanIka(-1);
+            // Annetaan Kollille negatiivinen ika konsturkorissa
+            Kolli.AsetaKissanIka(-1);
 
-            //// tulostaa boolean totuusarvon
-            //Console.WriteLine($"Negatiivisella iällä boolean totuusarvo on: {Kolli.AsetaKissanIka(-1)}");
+            // tulostaa boolean totuusarvon
+            Console.WriteLine($"Negatiivisella iällä boolean totuusarvo on: {Kolli.AsetaKissanIka(-1)}");
 
-            //// Annetaan Kollille uusi ika
-            //Kolli.AsetaKissanIka(18);
+            // Annetaan Kollille uusi ika
+            Kolli.AsetaKissanIka(18);
 
-            //// Tulostaa Kollin nimen ja ian
-            //Console.WriteLine($"{Kolli.PalautaKissanNimi()} on {Kolli.PalautaKissanIka()} on vuotta.");
+            // Tulostaa Kollin nimen ja ian
+            Console.WriteLine($"{Kolli.PalautaKissanNimi()} on {Kolli.PalautaKissanIka()} on vuotta.");
 
-            //// Luodaan uusi olio katti
-            //Kissa katti = new Kissa();
+            // Luodaan uusi olio katti
+            Kissa katti = new Kissa();
 
-            //// Annetaan nimeksi Anneli
-            //katti.AsetaKissanNimi("Anneli");
+            // Annetaan nimeksi Anneli
+            katti.AsetaKissanNimi("Anneli");
 
-            //// Tulostaa katti kissan nimen
-            //Console.WriteLine($"Katti kissan nimi on nyt {katti.PalautaKissanNimi()}.");
+            // Tulostaa katti kissan nimen
+            Console.WriteLine($"Katti kissan nimi on nyt {katti.PalautaKissanNimi()}.");
 
-            //// asetetaan nimeksi Hilda ja tulostaa boolean totuusarvon
-            //Console.WriteLine($"Hilda nimellä boolean totuusarvo on: {katti.AsetaKissanNimi("Hilda")}");
+            // asetetaan nimeksi Hilda ja tulostaa boolean totuusarvon
+            Console.WriteLine($"Hilda nimellä boolean totuusarvo on: {katti.AsetaKissanNimi("Hilda")}");
 
-            //// Override ToString()
-            //Console.WriteLine(Kolli);
+            // Override ToString()
+            Console.WriteLine(Kolli);
             Koira Turre = new Koira();
             Turre.AsetaKoiranNimi("Max");
             Console.WriteLine($"Koiran nimi on: {Turre.PalautaKoiranNimi()}");
